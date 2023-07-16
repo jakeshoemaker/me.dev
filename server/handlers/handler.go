832e2ae3431bc1e5ee40f75df297dbb14fbbef00
jake shoemaker <jakeshoe3@gmail.com>
2023-07-16 16:05:58 -0500

    theme switcher (little broke rn but functions)

--- server/handlers/handler.go
+++ server/handlers/handler.go
@@ -1,14 +1,14 @@
 package handlers
 
 import (
-    "embed"
-    "fmt"
-    "io/fs"
-    "net/http"
+	"embed"
+	"fmt"
+	"io/fs"
+	"net/http"
 
-    //"github.com/google/uuid"
-    "github.com/gorilla/mux"
-    "github.com/jakeshoemaker/me.dev/server/views"
+	//"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/jakeshoemaker/me.dev/server/views"
 )
 
 var (
@@ -20,13 +20,17 @@ type Handler struct {
     http.Handler
     //view *views.ModelView[models.View]
     index *views.IndexView
+    theme_view *views.ThemeView
+    darkMode bool
 }
 
-func CreateHandler(view *views.IndexView) (*Handler, error) {
+func CreateHandler(view *views.IndexView, theme_view *views.ThemeView) (*Handler, error) {
     router := mux.NewRouter()
     handler := &Handler {
         Handler: router,
         index: view,
+        theme_view: theme_view,
+        darkMode: true,
     }
 
     static_handler, err := createStaticHandler()
@@ -35,6 +39,7 @@ func CreateHandler(view *views.IndexView) (*Handler, error) {
     }
 
     router.HandleFunc("/", handler.get_index).Methods(http.MethodGet)
+    router.HandleFunc("/theme", handler.update_theme).Methods(http.MethodPut)
 	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", static_handler))
 
     return handler, nil
@@ -42,6 +47,16 @@ func CreateHandler(view *views.IndexView) (*Handler, error) {
 
 func (t *Handler) get_index(writer http.ResponseWriter, _ *http.Request) {
     t.index.Index(writer)
+}
+
+func (t *Handler) update_theme(writer http.ResponseWriter, _ *http.Request) {
+    if t.darkMode {
+        t.darkMode = false
+        t.theme_view.UpdateThemeIcon(writer, t.darkMode);
+    } else {
+        t.darkMode = true
+        t.theme_view.UpdateThemeIcon(writer, t.darkMode);
+    }
 }
 
 func createStaticHandler() (http.Handler, error) {
